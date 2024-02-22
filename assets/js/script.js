c92e7b4d179b58c05fa7f94da065a8d1e8e22b17
libap <limandribaptiste@gmail.com>
2024-02-22 14:02:37 +0100

    effet loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
 
 
 /**
